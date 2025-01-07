637bb3d929e24c8e7cc3d4536cf6b3d476a2da3d
Overtime <Truen2005@gmail.com>
2025-01-07 01:38:37 -0500

    stuff

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
