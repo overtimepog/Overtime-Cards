9f16c6052942bda8d19ce2229b7b0e1846ba8a0b
Overtime <Truen2005@gmail.com>
2025-01-10 18:54:32 -0500

    stuff

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -4,6 +4,8 @@ module.exports = {
     ['@babel/preset-env', { targets: { node: 'current' } }]
   ],
   plugins: [
-    '@babel/plugin-proposal-private-property-in-object'
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-class-properties', { loose: true }]
   ]
 }; 
